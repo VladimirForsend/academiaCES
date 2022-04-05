9cda347d830543766f540a81daa5b188bc353687
VladimirForsend <vladimir@forsend.cl>
2022-04-05 17:51:59 -0400

    agregar container fluid
    agregar container fluid al site main.

--- wp-content/themes/academiaCES/home-page.php
+++ wp-content/themes/academiaCES/home-page.php
@@ -18,7 +18,7 @@ else :
     get_header();
 endif;?>
    <?php include get_template_directory() . '/assets/modulos/modulo-slider/slider.php'; ?>
-	<main id="primary" class="site-main">
+	<main id="primary" class="site-main container-fluid">
 		<?php
 		while ( have_posts() ) :
 			the_post();
