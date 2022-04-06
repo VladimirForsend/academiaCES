7ec73ee6585cfdee84fd37d5d1aab2cc7c1d29c2
marcos <marcos@forsend.cl>
2022-04-06 15:37:06 -0400

    Create page-login.php
    nueva página

---
+++ wp-content/themes/academiaCES/page-login.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Template name: login CES
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package craed_duoc_uc
+ */
+
+if ( is_front_page() ) :
+    get_header( 'front' );
+else :
+    get_header();
+endif;?>
+   <?php include get_template_directory() . '/assets/modulos/modulo-slider/slider.php'; ?>
+	<main id="primary" class="site-main hola-LF">
+		<?php
+		while ( have_posts() ) :
+			the_post();
+
+			get_template_part( 'template-parts/content-home', 'page' );
+
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
+
+		endwhile; // End of the loop.
+		?>
+
+	</main><!-- #main -->
+
+<?php
+
+get_footer();
