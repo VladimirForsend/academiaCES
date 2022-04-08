6870e5e72832b3a4678d0249b0b890f7b2100c2a
marcos <marcos@forsend.cl>
2022-04-08 19:10:24 -0400

    Update page-login.php

--- wp-content/themes/academiaCES/page-login.php
+++ wp-content/themes/academiaCES/page-login.php
@@ -14,10 +14,32 @@
 
 	$user_login = 'marcos';
 
+
+	
+	$user_login = sanitize_user( $_POST['username'] );
+	$user_email = sanitize_user( $_POST['email']    );
+
+	
+
+
+
+	$user_id = username_exists( $user_login );
+
+	
+	if ( ! $user_id && false == email_exists( $user_email ) ) {
+		$random_password = wp_generate_password( $length = 12, $include_standard_special_chars = false );
+		$user_id = wp_create_user( $user_name, $random_password, $user_email );
+	}
+	
 	$user = get_userdatabylogin($user_login);
+	
 	$user_id = $user->ID;
+
 	wp_set_current_user($user_id, $user_login);
+
 	wp_set_auth_cookie($user_id);
 
 	do_action('wp_login', $user_login);
-get_footer();
+
+	
+//get_footer();
