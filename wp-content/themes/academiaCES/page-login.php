36547ac8e25618933cb981e866f80ca065010453
marcos <marcos@forsend.cl>
2022-04-19 15:27:09 -0400

    Update page-login.php

--- wp-content/themes/academiaCES/page-login.php
+++ wp-content/themes/academiaCES/page-login.php
@@ -9,13 +9,22 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
+ * @package craed_duoc_uc
  */
-	get_header();
-	$user_login = sanitize_user( $_GET['username'] );
-	$user_email = sanitize_user( $_GET['email']    );
 
-	echo $user_login;
-	echo $user_email;
+	$user_login = 'marcos';
+
+
+	
+	$user_login = sanitize_user( $_POST['username'] );
+	$user_email = sanitize_user( $_POST['email']    );
+
+	
+	$user_login = "10276571-0" ;
+	$user_email = "asunciona@gmail.com"   ;
+	//$user_login = "10276571-0" ;
+	//$user_email = "asunciona@gmail.com"   ;
+
 
 	$user_id = username_exists( $user_login );
 
@@ -23,35 +32,17 @@
 	if ( ! $user_id && false == email_exists( $user_email ) ) {
 		$random_password = wp_generate_password( $length = 12, $include_standard_special_chars = false );
 		$user_id = wp_create_user( $user_login, $random_password, $user_email );
-	}else{
-		
-		$user = get_userdatabylogin($user_login);	
-		$user_id = $user->ID;
 	}
-/*
+	
+	$user = get_userdatabylogin($user_login);
+
+	$user_id = $user->ID;
+
 	wp_set_current_user($user_id, $user_login);
 
 	wp_set_auth_cookie($user_id);
 
 	do_action('wp_login', $user_login);
-	// Automatic login //
-	*/
 
-	echo $user_id;
-	$user = get_user_by('login', $user_login );
-
-	// Redirect URL //
-	if ( !is_wp_error( $user ) )
-	{
-		clean_user_cache($user->ID);
-		wp_clear_auth_cookie();
-		wp_set_current_user($user->ID);
-		wp_set_auth_cookie($user->ID, true, false);
-		update_user_caches($user);
 	
-	}
-		?>
-
-	<script>
-		window.location = '<?php echo  get_home_url();  ?>';
-	</script>
+//get_footer();
