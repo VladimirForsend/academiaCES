4d5db58462bc638431618efd1afde6cd60c7b3aa
mabarza <marcos.abarza@mail.udp.cl>
2022-04-20 12:21:49 -0400

    Update page-login.php

--- wp-content/themes/academiaCES/page-login.php
+++ wp-content/themes/academiaCES/page-login.php
@@ -68,9 +68,10 @@
 
 
 	wp_update_user( $userdata ) ;
-	
-	update_user_meta( $user_id, '_lp_custom_register[7688225]', $user_cel );
-	update_user_meta( $user_id, '_lp_custom_register[6821613]', $user_nivel );
+	$user_nivel_solo = str_replace(".jpg","" , $user_nivel );
+
+	update_user_meta( $user_id, '_lp_custom_register','a:1:{i:7688225;s:9:"'.$user_cel.'";} ' );
+	update_user_meta( $user_id, '_lp_custom_register','a:1:{i:6821613;s:9:"'.$user_nivel_solo.'";} ' ); 
 	
 
 	wp_set_current_user($user_id, $user_login);
