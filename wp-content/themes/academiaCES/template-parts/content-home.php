8e61641c9c4c06f1b7c0ef45742389eaae8de8d6
Vladimir Torres Muñoz <102809673+VladimirForsend@users.noreply.github.com>
2022-06-16 11:34:52 -0400

    Update content-home.php

--- wp-content/themes/academiaCES/template-parts/content-home.php
+++ wp-content/themes/academiaCES/template-parts/content-home.php
@@ -12,9 +12,9 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
 
-<!-- seccion modulo de featured -->
-<?php// include get_template_directory() . '/assets/modulos/modulo-featured/featured-base.php'; ?>
-<!-- seccion modulo de featured -->
+<!-- seccion modulo de featured
+<?php // include get_template_directory() . '/assets/modulos/modulo-featured/featured-base.php' ;?>
+seccion modulo de featured -->
 <!-- seccion modulo aprender -->
 <?php include get_template_directory() . '/assets/modulos/modulo-aprender/aprenderas.php'; ?>
 <!-- seccion modulo aprender -->
